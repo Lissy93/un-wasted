9654018be05e7d99e7024be678318abf59394db1
Alicia Sykes <sykes.alicia@gmail.com>
2018-02-24 12:24:38 +0000

    rn endpoint 'products' --> 'find-food' (for Ollie), and rm .idea (for Ted)

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var products = require('./routes/products');
+var findFood = require('./routes/find-food');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/products', products);
+app.use('/find-food', findFood);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
